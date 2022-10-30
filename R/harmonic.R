59c7dea806197a6cdc94d047908aab772f8fe937
Jason Everett <jaseeverett@gmail.com>
2022-10-31 10:46:46 +1100

    Update documentation

--- R/harmonic.R
+++ R/harmonic.R
@@ -1,17 +1,19 @@
-#
-
-#' Harmonic is to fit Hour and DOY
+#' For use in models to create a circular predictor
 #'
-#' @param theta
-#' @param k
+#' This function is for use in models to create a circular predictor
+#' @param theta Paramter to model in radians
+#' @param k degrees of freedom
 #'
-#' @return
+#' @return A harmonic function
 #' @export
 #'
 #' @examples
+#' theta = 2
+#' k = 1
+#' df <- pr_harmonic(theta = 2, k = 1)
 harmonic <- function (theta, k = 4) {
   X <- matrix(0, length(theta), 2 * k)
-  nam <- as.vector(outer(c("c", "s"), 1:k, paste, sep = ""))
+  nam <- as.vector(outer(c("c", "s"), 1:k, paste, sep  = ""))
   dimnames(X) <- list(names(theta), nam)
   m <- 0
   for (j in 1:k) {
