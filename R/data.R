9624ac9620a5aa99ca33c55629dfd868dd991453
Jason Everett <jaseeverett@gmail.com>
2023-08-12 15:29:00 +1000

    Update data

---
+++ R/data.R
@@ -0,0 +1,24 @@
+#' Longhurst Provinces
+#'
+#' An sf dataframe of
+#'
+#' @format `longhurst`
+#' A data frame with XXXX rows and XXX columns:
+#' \describe{
+#' }
+#' @source "XXXXXX"
+"longhurst"
+
+
+# -------------------------------------------------------------------------
+
+#' MEOW
+#'
+#' An sf dataframe of
+#'
+#' @format `meow`
+#' A data frame with XXXX rows and XXX columns:
+#' \describe{
+#' }
+#' @source "XXXXXX"
+"meow"
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,6 +1,9 @@
 ## code to prepare `DATASET` dataset goes here
 
-longhurst <- sf::st_read(file.path("data-raw", "longhurst_v4_2010","longhurst_world_v4_2010.shp"))
-meow <- sf::st_read(file.path("data-raw", "MEOW","meow_ecos.shp"))
+longhurst <- sf::st_read(file.path("data-raw", "longhurst_v4_2010","longhurst_world_v4_2010.shp")) %>%
+  sf::st_make_valid()
+
+meow <- sf::st_read(file.path("data-raw", "MEOW","meow_ecos.shp")) %>%
+  sf::st_make_valid()
 
 usethis::use_data(longhurst, meow, overwrite = TRUE)
